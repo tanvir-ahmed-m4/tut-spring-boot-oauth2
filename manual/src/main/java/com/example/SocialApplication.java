c346c821b9e45f6fd039532c5cfe2a1b3f4762ae
Tanvir Ahmed <tanvir.ahmed@metafour.com>
2017-07-10 23:30:31 +0600

    Testing Google with RestTemplate wire logging

--- manual/src/main/java/com/example/SocialApplication.java
+++ manual/src/main/java/com/example/SocialApplication.java
@@ -15,10 +15,17 @@
  */
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.Filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,6 +34,8 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoT
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -81,26 +90,77 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 	private Filter ssoFilter() {
 		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
 				"/login/facebook");
+//				"/login/google");
 		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+
+//		facebookTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+//		facebookTemplate.setRequestFactory(new HttpComponentsAsyncClientHttpRequestFactory());
+//		facebookTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+		facebookTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory()));
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>();
+		interceptors.add(new LoggingRequestInterceptor());
+		facebookTemplate.setInterceptors(interceptors);
+
 		facebookFilter.setRestTemplate(facebookTemplate);
 		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(),
 				facebook().getClientId());
 		tokenServices.setRestTemplate(facebookTemplate);
-		facebookFilter.setTokenServices(
-				new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
+		facebookFilter.setTokenServices(tokenServices);
 		return facebookFilter;
 	}
 
 	@Bean
 	@ConfigurationProperties("facebook.client")
+//	@ConfigurationProperties("google.client")
 	public AuthorizationCodeResourceDetails facebook() {
 		return new AuthorizationCodeResourceDetails();
 	}
 
 	@Bean
 	@ConfigurationProperties("facebook.resource")
+//	@ConfigurationProperties("google.resource")
 	public ResourceServerProperties facebookResource() {
 		return new ResourceServerProperties();
+	}
+
+}
+
+class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
+
+	final static Logger log = LoggerFactory.getLogger(LoggingRequestInterceptor.class);
+
+	@Override
+	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+		traceRequest(request, body);
+		ClientHttpResponse response = execution.execute(request, body);
+		traceResponse(response);
+		return response;
+	}
+
+	private void traceRequest(HttpRequest request, byte[] body) throws IOException {
+		log.debug("===========================request begin================================================");
+		log.debug("URI         : {}", request.getURI());
+		log.debug("Method      : {}", request.getMethod());
+		log.debug("Headers     : {}", request.getHeaders() );
+		log.debug("Request body: {}", new String(body, "UTF-8"));
+		log.debug("==========================request end================================================");
+	}
+
+	private void traceResponse(ClientHttpResponse response) throws IOException {
+		StringBuilder inputStringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody(), "UTF-8"));
+		String line = bufferedReader.readLine();
+		while (line != null) {
+			inputStringBuilder.append(line);
+			inputStringBuilder.append('\n');
+			line = bufferedReader.readLine();
+		}
+		log.debug("============================response begin==========================================");
+		log.debug("Status code  : {}", response.getStatusCode());
+		log.debug("Status text  : {}", response.getStatusText());
+		log.debug("Headers      : {}", response.getHeaders());
+		log.debug("Response body: {}", inputStringBuilder.toString());
+		log.debug("=======================response end=================================================");
 	}
 
 }
